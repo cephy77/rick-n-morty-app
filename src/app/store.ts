64fd6c7232050d2cac2bd55119eb7b1fadeb9bac
Oleksii <nano77@ukr.net>
2023-11-03 01:05:31 +0200

    add store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import charactersSlice from '../features/characters/charactersSlice';
 
 export const store = configureStore({
   reducer: {
-    // characters: charactersSlice,
+    characters: charactersSlice,
   }
 });
 
