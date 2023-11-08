7dc544f95a407538f1e892b627911e203ff5f4f3
Oleksii <nano77@ukr.net>
2023-11-08 17:22:58 +0200

    change to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './app/store.ts'
 import './index.scss'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <React.StrictMode>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>
   </Provider>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://cephy77.github.io/rick-n-morty-app/',
   plugins: [react()],
 })
